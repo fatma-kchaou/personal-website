9dcf9686116288e104d1c2720218b8a4dbcd8b72
fatma kchaou <159474758+fatma-kchaou@users.noreply.github.com>
2024-12-07 16:40:24 +0100

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,31 @@
+function verif() { 
+    var nom = document.form.nom.value; 
+    // var nom = document.getElementByID("nom").value; 
+    var prenom = document.form.prenom.value; 
+    var message=document.form.message.value; 
+    if(nom=="") {
+        alert('Ajouter votre Nom !');
+        document.form.nom.focus(); 
+      return false;
+
+    } 
+
+  if(prenom=="") {
+        alert('Ajouter votre Prénom !\n');
+        document.form.prenom.focus(); 
+        return false;
+
+    } 
+    if(message=="") {
+        alert('Ajouter La Date !\n');
+        document.form.message.focus(); 
+        return false;
+
+        
+    } 
+   
+ }
+ 
+
+
+
